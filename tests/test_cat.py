8c1df54d403ac19801e442f820ff4dc1a680bccb
joelvalladolid <148713821+joelvalladolid@users.noreply.github.com>
2026-06-03 19:30:43 -0500

    delete

--- tests/test_cat.py
+++
@@ -1,8 +0,0 @@
-﻿import sys
-sys.path.insert(0, r"c:\Users\Alumno\Desktop\SMF\revolution-main\REVOLUTION_ENTREGABLE")
-from app import analyze_ticker_for_today
-res = analyze_ticker_for_today('CAT', 'CALM', 4.2, '2023-01-01', '2026-01-01', False, 'MOMENTUM')
-if res:
-    print("CAT RSI:", res['ind_vals'].get('RSI'))
-    print("CAT Score:", res['signal']['confidence'])
-    print("CAT Signal:", res['signal']['signal'])
